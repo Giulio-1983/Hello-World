7480f02c78e9e642b761b8ac79343c3200841f4b
IrynaMe <irinam7a@gmail.com>
2024-04-23 17:09:05 +0200

    commit Iryna

--- ProvaProgetto/src/ProvaGithub/HelloGit.java
+++ ProvaProgetto/src/ProvaGithub/HelloGit.java
@@ -1,10 +1,12 @@
 package ProvaGithub;
 
 public class HelloGit {
+	//modifica da Iryna
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello GitHub");
+		System.out.println("hello from Github collaborator");
 	}
 
 }
